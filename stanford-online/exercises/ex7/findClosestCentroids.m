84210b8714e177d9ddf7ebf17dfc6ea2e2933c79
Prashant Mishra <prash_mi@yahoo.com>
2020-05-25 22:33:31 +0530

    ex7 - KMeans & PCA

---
+++ stanford-online/exercises/ex7/findClosestCentroids.m
@@ -0,0 +1,59 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+%for all the points check which centroid is the closest and get the closest centroid's index in idx
+for i = 1:length(idx)
+
+minDis = Inf; 
+	
+    for j = 1:K
+        
+        curDis = sum(sum((X(i, :) - centroids(j, :)) .^ 2 ));
+		
+			%vectorised approach could be as follows 
+		    
+			%diff = X(i, :)'-centroids(k, :)';
+			%distance = diff'*diff;
+		if curDis < minDis
+		
+			idx(i) = j; %jth centroid is closest to the ith point
+			minDis = curDis;
+		
+		end
+	
+    end
+	
+  
+	
+end
+
+%idx
+
+%fprintf('Program paused @findClosestCentroids Press enter to continue.\n');
+%pause;
+
+
+% =============================================================
+
+end
+
